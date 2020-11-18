3fcc73ffbd9c50fff1d94750ca3ce2961ff5f829
Maëlys <61282146+Miyou-da@users.noreply.github.com>
2020-11-18 15:01:42 +0100

    Update Condorcet.py

--- Condorcet.py
+++ Condorcet.py
@@ -1,51 +1,69 @@
 import random as rd
 import matplotlib.pyplot as plt
 
+#variables globales
 partis_pos = [0, 1/6, 2/6, 3/6, 4/6, 5/6, 1] #positions des partis sur l'échelle politique
-partis = ["extreme gauche", "gauche", "centre gauche", "centre", "centre droit", "droite", "extreme droite"]
+partis = ["extreme gauche", "gauche", "centre gauche", "centre", "centre droit", "droite", "extreme droite"] #liste des partis
 
 pop1 = [rd.gauss(mu=5/6, sigma=0.15) for k in range(100000)]
 pop0 = [rd.gauss(mu=1/6, sigma=0.15) for k in range(100000)]
-pop = pop0 + pop1
+pop = pop0 + pop1 #population de travail
 
-pop_parti=[0 for k in range(7)]
+pop_parti=[0 for k in range(7)] 
+
+#Fonction globale
+def trouve_parti(individu, liste_partis):
+    ##Trouve un parti adapté à un individu
+    if individu < liste_partis[0]:
+        #Si l'individu est plus à gauche que le parti le plus à gauche, on renvoie le parti le plus à gauche.
+        return 0
+    if individu > liste_partis[-1]:
+        #Si l'individu est plus à droite que le parti le plus à droite, on renvoie le parti le plus à droite.
+        return len(liste_partis)-1
+    for position in range(len(liste_partis)):
+        #On fait défiler les partis de gauche à droite
+        parti = liste_partis[position]
+        if individu > parti:
+            #On vérifie que l'individu est bien à droite du parti, sinon son cas aurait dû être traîté dans les boucles précédentes
+            suivant = liste_partis[position+1]
+            if individu < suivant:
+                #Si l'individu est bien entre le parti étudié et le suivant,
+                if individu < (parti + suivant)/2:
+                    #Si l'individu est plus proche du parti étudié que du suivant, on renvoit la position de ce parti
+                    return position
+                else:
+                    #Sinon on renvoit la position de l'autre
+                    return position+1
+
+"""
+Condorcet
+"""
 
 def scrutins():
-    #creer le scrutins (les duels)
+    ##crée le scrutins (les duels)
     scrutin = []
     for i in range(len(partis)) :
         for j in range((i+1), len(partis)) :
+            #On fait en sorte que chaque parti rencontre tous les autres
             scrutin.append((i,j))
     return scrutin
 
-def trouve_parti(individu, liste_partis):
-    #trouve un parti adapté à un individu
-    if individu < liste_partis[0]:
-        return 0
-    if individu > liste_partis[-1]:
-        return len(liste_partis)-1
-    for pos in range(len(liste_partis)):
-        valbasse = liste_partis[pos]
-        if individu > valbasse:
-            valhaute = liste_partis[pos+1]
-            if individu < valhaute:
-                if individu < (valbasse+valhaute)/2:
-                    return pos
-                else:
-                    return pos+1
-
 def cree_bulletins(individu, scrutin):
+    #fait voter un individu (crée son bulletin de vote)
     bulletin = []
     for duel in scrutin:
+        #Pour chaque duel,
         val1, val2 = duel
         lvalduel = [partis_pos[val1], partis_pos[val2]]
         vainc = trouve_parti(individu, lvalduel)
+        #On cherche le parti le plus proche de l'individu (renvoit 0 si c'est celui de gauche et 1 si c'est celui de droite
         bulletin.append(vainc == 0)
+        #On compare le vainqueur 
     return bulletin
 
 
 def duel_condorcet(bulletins, scrutin):
-     #on dépouille les bulletins
+    #Dépouille les bulletins
     score = [0 for k in partis]
     for bulletin in bulletins:
         for nduel in range(len(bulletin)):
@@ -55,7 +73,7 @@ def duel_condorcet(bulletins, scrutin):
                 score[duel1] += 1
             else :
                 score[duel2] += 1
-    #On trouve le ou les vainqueurs
+    #Trouve le ou les vainqueurs
     score_vainqueur = max(score)
     vainqueur =[]
     for i in range (len(partis)):
@@ -64,12 +82,14 @@ def duel_condorcet(bulletins, scrutin):
     return (vainqueur, score)
 
 def main_condorcet():
+    #Réalise un vote de Condorcet, renvoie juste le ou les vainnqueurs
     scrutin = scrutins()
     bulletins = [cree_bulletins(individu, scrutin) for individu in pop]
     vainqueur, score = duel_condorcet(bulletins, scrutin)
     return vainqueur
 
 def main_condorcet_graph():
+    #Réalise un vote de Condorcet, renvoie le ou les vainqueurs et un graph des résultats du vote
     scrutin = scrutins()
     bulletins = [cree_bulletins(individu, scrutin) for individu in pop]
     vainqueur, score = duel_condorcet(bulletins, scrutin)
@@ -89,13 +109,15 @@ Uninominal
 """
 
 def cree_bulletins_uninominal(individu):
+    #Fait voter un individu (crée un bulletin de vote uninominal)
     return trouve_parti(individu, partis_pos)
 
 def vote_uninominal(bulletins):
+    #Dépouille les bulletins
     score = [0 for k in partis]
     for vote in bulletins:
         score[vote] += 1
-    #On trouve le vainqueur
+    #Trouve le vainqueur
     score_vainqueur = max(score)
     vainqueur = []
     for i in range(len(partis)):
@@ -104,6 +126,7 @@ def vote_uninominal(bulletins):
     return (vainqueur, score)
 
 def main_uninominal():
+    #Réalise le vote uninominal et renvoit le ou les vainqueurs
     bulletins = [cree_bulletins_uninominal(individu) for individu in pop]
     vainqueur, score = vote_uninominal(bulletins)
     plt.clf()
@@ -121,6 +144,7 @@ def main_uninominal():
 Alternatif
 """
 def cree_bulletins_alternatif(individu):
+    #Fait voter un individu (crée un bulletin de vote alternatif)
     liste_choix = partis_pos.copy()
     n = len(liste_choix)
     bulletin = [0 for k in liste_choix]
